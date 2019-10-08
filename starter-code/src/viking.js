7466286acc4a781738c71130f3818e7aa9bedda5
Anaïs Person <anaisperson@MacBook-de-Anais.local>
2019-10-08 21:40:01 +0200

    undone

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,39 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        this.health >= 0;
+        return `${this.name} has received ${this.damage} of damage`;
+
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
